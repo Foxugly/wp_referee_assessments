b133566a3eb53511f2de6c598ff215a0a8df30bd
Foxugly <rvilain@skynet.be>
2019-02-19 11:00:54 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -37,6 +37,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    urlpatterns += [ path('__debug__/', include(debug_toolbar.urls)), ]
