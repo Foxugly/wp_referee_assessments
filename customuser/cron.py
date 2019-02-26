83fe71bdd300e3d69a5693580e26e06286948217
Foxugly <rvilain@skynet.be>
2019-02-26 16:21:08 +0100

    Update cron.py

--- customuser/cron.py
+++ customuser/cron.py
@@ -1,4 +1,18 @@
+from customuser.models import CustomUser
+from assessment.models AssessmentMatch
 
 
 def send_mail():
-  pass
+  ams = AssessmentMatch.objects.filter(sent=False)
+  # il faut checker la date
+  for am in ams:
+    usersHome = CustomUser.objects.filter(teams__in=am.match.teamH, categories__in=am.match.competition.category)
+    for uh is usersHome:
+      print(uh)
+      #send mail
+    usersAway = CustomUser.objects.filter(teams__in=am.match.teamA, categories__in=am.match.competition.category)
+    for ua is usersAway:
+      print(ua)
+      #send mail
+    am.sent = True
+    am.save()
